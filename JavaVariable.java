28f52f4e01bd502e34d2db7ec16c9f6aca5a5862
giasuddin2548 <giasuddin2548@gmail.com>
2020-09-19 01:07:30 +0600

    initial commit

---
+++ JavaVariable.java
@@ -0,0 +1,34 @@
+
+package basic.java;
+
+public class JavaVariable {
+
+
+ 
+    public static void main(String[] args) {
+   
+        int balance=500; //initialze variable
+        int amount=50;
+        
+        int add;   
+        double mutliplication;
+        double divition;
+        int subtraction;
+        
+        add=balance+amount;
+        
+        subtraction=balance-amount;
+        
+        //samrt way to declaration of a variable
+        int a,b,c,d=10;
+
+        System.out.println("Addition:"+add);
+        System.out.println(balance+amount);
+        
+        
+        
+        
+        
+    }
+    
+}
